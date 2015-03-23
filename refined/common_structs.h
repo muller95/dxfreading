57ce38834d2bac64fb9016938157613f25bbc231
definedwheel <andronwheel@gmail.com>
2015-03-23 22:58:51 +0300

    Very first sketches for new structs
    I think we need new structs arch due to bloated and uneffective prototype ones.
    For instance, whats the difference between DxfPrimitive and LinearPrimitive? Only in types?
    If so, why don't we have one struct? Many questions, gonna think about them.

---
+++ refined/common_structs.h
@@ -0,0 +1,34 @@
+
+/* Data structures for all functions */
+
+#define DXF_TYPE_LINE 0
+#define DXF_TYPE_SPLINE 1
+
+struct Point {
+  double x;
+  double y;
+}
+
+struct DxfPrimitive {
+  struct Point *points;
+  int type;
+  int controldots;
+}
+
+struct LinearPrimitive {
+  struct Point *points;
+  int type;
+}
+
+struct DxfFile {
+  char *path;
+  int how_many;
+  struct DxfPrimitive *primitives;
+/*  Не помню, как назывались, и для чего нужны
+  double my_height;
+  double my_width;
+*/
+  struct Point min;
+  struct Point max;
+}
+
